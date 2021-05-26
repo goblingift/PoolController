07cefc644c48e326b771482ff99dc13bf97a9158
André Keßler <andre.kessler91@gmx.de>
2021-05-26 19:04:45 +0200

    First commit of PoolController

---
+++ libraries/DisplayImages/DisplayImages.h
@@ -0,0 +1,17 @@
+/*
+  DisplayImages.h - Library for displaying images for lcd display.
+  Created by Andre Kessler (goblingift), May 23, 2021.
+*/
+#ifndef DisplayImages_h
+#define DisplayImages_h
+
+#include "Arduino.h"
+
+class DisplayImages
+{
+  public:
+    DisplayImages();
+    char* getGoblingiftLogo();
+};
+
+#endif
